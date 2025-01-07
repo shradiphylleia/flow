ef1470888d602b34d379bed258de1317ee922eeb
shradiphylleia <shradulsha@gmail.com>
2025-01-07 18:47:19 +0530

    symptoms selecetion

--- flow/src/Try.jsx
+++ flow/src/Try.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './Try.css'
 
 function Try() {
   const [selectedSymptoms, setSelectedSymptoms] = useState([]);
@@ -28,13 +29,16 @@ function Try() {
 
   return (
     <div>
-      <h2>Select your concerns</h2>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
+      <div className='title'>
+        <h2>Select your concerns</h2>
+        </div>
+      <div className='concern'>
         {symptoms.map((symptom) => (
           <button
             key={symptom}
             onClick={() => handleSymptomClick(symptom)}
             style={{
+              marginLeft:'12px',
               padding: '10px',
               border: '1px solid #ccc',
               borderRadius: '5px',
@@ -51,17 +55,22 @@ function Try() {
         onClick={handleDone}
         style={{
           marginTop: '20px',
-          padding: '10px 20px',
+          marginLeft:'66vw',
+          padding: '10px 36px',
           backgroundColor: '#007bff',
           color: '#fff',
           border: 'none',
           borderRadius: '5px',
           cursor: 'pointer',
-        }}
-      >
+        }}>
         Done
       </button>
+      <div className="flowerconcernpage">
+      <img src="src/assets/blueflower.png" alt="flower" style={{marginLeft:'68vw', transform: 'rotate(-24deg)', position: 'absolute', width:'28vw'}} />
+
+      </div>
     </div>
+
   );
 }
 
