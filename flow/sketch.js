976ce7218f5d85ed274ddda38c964204ab9b4e33
shradiphylleia <shradulsha@gmail.com>
2025-01-10 14:57:30 +0530

    animated circle and lays on the presssurePoint completely need navigation to share state between pages about the choosen  symptoms

--- flow/sketch.js
+++ flow/sketch.js
@@ -1,8 +1,7 @@
+
 let handPose;
 let video;
 let hands = [];
-
-//have a trigger:true/false then go with the camera on and then allow for drawing then json and then advance
 
 function preload() {
   handPose = ml5.handPose();
@@ -16,29 +15,30 @@ function setup() {
   handPose.detectStart(video, gotHands);
 }
 
-function draw() {
-  // Draw the webcam video
-  image(video, 0, 0, width, height);
 
-  // Draw all the tracked hand points
+ let t = 0;
+
+function draw() {
+  image(video, 0, 0, width, height);
+  let pressurePoint = [0, 1, 2];
+
   for (let i = 0; i < hands.length; i++) {
     let hand = hands[i];
-    for (let j = 0; j < hand.keypoints.length; j++) {
-      let keypoint = hand.keypoints[j];
-      fill(0, 255, 0);
-      noStroke();
-      circle(keypoint.x, keypoint.y, 10);
+    for (let j = 0; j < pressurePoint.length; j++) {
+      let k = pressurePoint[j];
+      let keypoint = hand.keypoints[k];
+      
+      let animatedRadius = 5 + 4 * sin(t);
+
+      fill(55, 111, 115);
+      circle(keypoint.x, keypoint.y, animatedRadius);
     }
   }
+
+  t += 0.1;
 }
 
-// Callback function for when handPose outputs data
+
 function gotHands(results) {
-  // save the output to the hands variable
   hands = results;
 }
-
-function modelLoaded() {
-  console.log("Handpose model loaded!");
-}
-
--- flow/src/Assist.jsx
+++ flow/src/Assist.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Assist() {
   return (
-    <div>Assist</div>
+    <div>
+      <h1>Hand Landmark Assist</h1>
+      <p>Real-time hand tracking with highlighted landmarks.</p>
+    </div>
   )
 }
 
